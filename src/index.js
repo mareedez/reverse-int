a03578dd6d76719d65686ba015d4ec7ad89334b5
mareedez <xxl_century@yahoo.com>
2022-01-05 18:11:31 -0800

    feat: adeed solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function reverse (n) {
     n = String(n)
   if (n.startsWith("-")) {
-    return -(n.slice(1).split("").reverse().join(""))
+    return +(n.slice(1).split("").reverse().join(""))
   } else {
     return +(n.split("").reverse().join(""))
   }
